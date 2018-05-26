cb64e3393163305487aedba74c7f2b8179ae18ba
vikas <vikaspal807657@gmail.com>
2018-05-26 10:22:19 -0700

    create database connecetion in books_db.php

--- books_db.php
+++ books_db.php
@@ -6,26 +6,26 @@
 	$db_name = "brm_db";
 
 	$conn = mysqli_connect($host,$user,$password,$db_name);
-	if(!$conn){
-		echo "connection error";
-	}else{
-		echo "connect successfully";
+	 if(!$conn){
+	 	echo "connection error";
+	 }else{
+	 	echo "connect successfully";
 	}
 
-	$table = "book";
-	$create_table = "CREATE TABLE book(
-						BookID int(5) PRIMARY KEY AUTO_INCREMENT,
+
+	 $table = "book";
+	 $create_table = "CREATE TABLE book(
+	 					BookID int(5) PRIMARY KEY AUTO_INCREMENT,
 						Title varchar(255) NOT NULL,
 						Price Float(7,2) NOT NULL,
 						Author varchar(255)
 						)";
 
-	if(mysqli_query($conn,$create_table)){
+	 if(mysqli_query($conn,$create_table)){
 	 	echo "table creted";
-	}
-	else
-		echo "table not created";
-
+	 }
+	 else
+	 	echo "table not created";
 
 
 
