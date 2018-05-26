b7f6fbb1efdb8377441817c514724122a806f685
vikas <vikaspal807657@gmail.com>
2018-05-26 09:08:42 -0700

    form created in insertForm.php and books_db created for database

---
+++ books_db.php
@@ -0,0 +1,34 @@
+<?php 
+
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+	$db_name = "brm_db";
+
+	$conn = mysqli_connect($host,$user,$password,$db_name);
+	if(!$conn){
+		echo "connection error";
+	}else{
+		echo "connect successfully";
+	}
+
+	$table = "book";
+	$create_table = "CREATE TABLE book(
+						BookID int(5) PRIMARY KEY AUTO_INCREMENT,
+						Title varchar(255) NOT NULL,
+						Price Float(7,2) NOT NULL,
+						Author varchar(255)
+						)";
+
+	if(mysqli_query($conn,$create_table)){
+	 	echo "table creted";
+	}
+	else
+		echo "table not created";
+
+
+
+
+
+
+ ?>
---
+++ insertForm.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Book</title>
+</head>
+<body>
+	<h1 align="center">Book Record Management</h1>
+	<form action="insert.php" method="post">
+		<table align="center">
+			<tr>
+				<td><th>Book Title</th></td>
+				<td><input type="text" name="book_title" required ></td>
+			</tr>
+			<tr>
+				<td><th>price</th></td>
+				<td><input type="text" name="price" required ></td>
+
+			</tr>
+			<tr>
+				<td><th>Author Name</th></td>
+				<td><input type="text" name="author_name" ></td>
+
+			</tr>
+			<tr><div>
+				<td></td>
+				<td colspan="3" align="center"><input type="submit" name="submit"></td>
+				
+			</div>
+				
+			</tr>
+			
+			
+
+
+
+
+		</table>
+		
+
+
+
+
+	</form>
+
+</body>
+</html>
